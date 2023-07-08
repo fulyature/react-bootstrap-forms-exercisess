26ecb62f5b9f2512b38094667d0657b576172ade
fulya_ture <fulyature@gmail.com>
2023-07-08 18:18:59 +0300

    project initalize

---
+++ src/1-forms/Form.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form = () => {
+  return <div className="text-danger">Form</div>;
+};
+
+export default Form;
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Form from "./1-forms/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
